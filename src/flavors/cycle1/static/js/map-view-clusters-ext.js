64d9e68281f519c987176d8b81f262989893f0e7
Mjumbe Poe <mjumbewu@gmail.com>
2024-05-05 17:34:07 -0400

    Add comments describing the changes in the map-view-clusters-ext module

--- src/flavors/cycle1/static/js/map-view-clusters-ext.js
+++ src/flavors/cycle1/static/js/map-view-clusters-ext.js
@@ -1,4 +1,16 @@
+// Add clustering to the map view
+// ==============================
+//
+// This script extends the Shareabouts map view to use clustering for the place
+// markers. It uses the Leaflet.markercluster plugin to do this.
+//
+// The script also adds a new layer group for focused places, which are places
+// that are currently being viewed in the detail view.
+
 (function() {
+
+  // Override the map view's initialize method to add the clustering layer
+  // and the focused place layer.
   var Shareabouts_MapView_initialize = Shareabouts.MapView.prototype.initialize;
   Shareabouts.MapView.prototype.initialize = function() {
     Shareabouts_MapView_initialize.apply(this, arguments);
@@ -16,12 +28,16 @@
     this.map.addLayer(this.focusedPlaceLayers);
   }
 
+  // Override the map view's render method to bring the focused place layer to
+  // the front.
   var Shareabouts_MapView_render = Shareabouts.MapView.prototype.render;
   Shareabouts.MapView.prototype.render = function() {
     Shareabouts_MapView_render.apply(this, arguments);
     this.focusedPlaceLayers.bringToFront();
   }
 
+  // Override the map view's addLayerView method to include the focused place
+  // layer in the layer views' options.
   var Shareabouts_MapView_addLayerView = Shareabouts.MapView.prototype.addLayerView;
   Shareabouts.MapView.prototype.addLayerView = function(model) {
     this.layerViews[model.cid] = new Shareabouts.LayerView({
@@ -35,6 +51,8 @@
     });
   }
 
+  // Override the layer view's show method to add the layer to the place layer
+  // or the focused place layer depending on whether the layer is focused.
   var Shareabouts_LayerView_show = Shareabouts.LayerView.prototype.show;
   Shareabouts.LayerView.prototype.show = function() {
     var locationTypeFilter = this.getLocationTypeFilter();
@@ -52,6 +70,8 @@
     }
   }
 
+  // Override the layer view's removeLayer method to remove the layer from the
+  // focused place layer in addition to the place layer.
   var Shareabouts_LayerView_removeLayer = Shareabouts.LayerView.prototype.removeLayer;
   Shareabouts.LayerView.prototype.removeLayer = function() {
     if (this.layer) {
