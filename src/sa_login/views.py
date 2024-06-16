eb45b7ad7005ed6b5094160ecf2876eb44ac0e92
Mjumbe Poe <mjumbewu@gmail.com>
2024-06-15 23:05:29 -0400

    Accept a "next" URL for logging in

--- src/sa_login/views.py
+++ src/sa_login/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from sa_util.config import get_shareabouts_config
 from sa_util.api import ShareaboutsApi, ShareaboutsApiError
 
@@ -17,6 +17,7 @@ def login(request: HttpRequest):
     if request.method == 'GET':
         api_user = api.current_user()
         api_cookie = api.sessionid
+        next_url = request.GET.get('next', None)
 
     # POST a new user session to log in to the API
     elif request.method == 'POST' and request.POST.get('shadowmethod', '').upper() != 'DELETE':
@@ -26,6 +27,7 @@ def login(request: HttpRequest):
             api_cookie = api.sessionid
         except ShareaboutsApiError as exc:
             error_str = f'Login failed. {"; ".join(exc.errors.values()) if exc.errors else "Please try again."}'
+        next_url = request.POST.get('next', None)
 
     # DELETE the current user session to log out of the API
     elif request.method == 'POST' and request.POST.get('shadowmethod', '').upper() == 'DELETE':
@@ -35,14 +37,20 @@ def login(request: HttpRequest):
             api_cookie = api.sessionid
         except ShareaboutsApiError:
             error_str = 'Failed to log out. Please try again.'
+        next_url = request.POST.get('next', None)
 
-    response = render(request, 'sa_login.html', {
-        'api_user': api_user,
-        'errors': error_str,
-        'config': config,
-        'dataset_root': api.dataset_root,
-        'auth_root': api.auth_root,
-    })
+    if api_user and next_url is not None:
+        response = redirect(next_url)
+
+    else:
+        response = render(request, 'sa_login.html', {
+            'api_user': api_user,
+            'errors': error_str,
+            'config': config,
+            'dataset_root': api.dataset_root,
+            'auth_root': api.auth_root,
+            'next_url': next_url,
+        })
 
     if api_cookie:
         response.set_cookie('sa-api-sessionid', api_cookie)
