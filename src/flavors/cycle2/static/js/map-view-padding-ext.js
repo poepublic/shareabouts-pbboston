18d621354edb376ea6eac515565178836eb2706a
Mjumbe Poe <mjumbewu@gmail.com>
2025-05-12 21:05:33 -0400

    Enhance form and overview content with demographic and contact info sections, improve styling, and update map padding logic

--- src/flavors/cycle2/static/js/map-view-padding-ext.js
+++ src/flavors/cycle2/static/js/map-view-padding-ext.js
@@ -8,13 +8,13 @@
     const map = Shareabouts_MapView_constructMap.apply(this, arguments);
     const view = this;
 
-    // Override the map's setView method to add padding to the center of the map.
+    // Override the map's getSize method to add padding to the map.
     // https://github.com/Leaflet/Leaflet/blob/v1.3.4/src/map/Map.js#L856
     map.getSize = function () {
       if (!this._size || this._sizeChanged) {
         this._contentPadding = view.contentPadding();
 
-        this._size = new L.Point(
+        this._size = L.point(
           (this._container.clientWidth || 0) - this._contentPadding.x,
           (this._container.clientHeight || 0) - this._contentPadding.y);
   
@@ -39,10 +39,9 @@
       return L.point(0, 0);
     }
 
-    // Get the width of the content panel.
+    // Account for the width of the visible content panel.
     const contentPanel = document.getElementById('content');
     const contentPanelWidth = contentPanel.offsetWidth;
-    
     return L.point(contentPanelWidth, 0);
   }
 })();
