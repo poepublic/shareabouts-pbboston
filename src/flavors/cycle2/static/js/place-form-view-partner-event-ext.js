c234891e9ff88ea053551dc72e92b30385418648
Mjumbe Poe <mjumbewu@gmail.com>
2025-06-03 02:22:36 -0400

    Add detail fields for "other" options

--- src/flavors/cycle2/static/js/place-form-view-partner-event-ext.js
+++ src/flavors/cycle2/static/js/place-form-view-partner-event-ext.js
@@ -6,37 +6,44 @@
 
 (function() {
 
-  const eventTypeFieldName = 'submission_event_type';
-  const eventOrgFieldName = 'submission_event_org';
-
   Shareabouts.PlaceFormView.prototype.events = {
     ...Shareabouts.PlaceFormView.prototype.events,
-    [`change input[name="${eventTypeFieldName}"]`]: 'onSubmissionEventTypeChange',
+    [`change [data-has-detail-children]`]: 'onDetailParentChange',
   };
 
-  Shareabouts.PlaceFormView.prototype.updateSubmissionEventFields = function() {
-    const eventTypeInput = this.el.querySelector(`input[name="${eventTypeFieldName}"]:checked`);
-    const eventOrgInput = this.el.querySelector(`[name="${eventOrgFieldName}"]`);
-    const eventOrgFieldWrapper = this.el.querySelector(`.place-${eventOrgFieldName}-field`);
+  Shareabouts.PlaceFormView.prototype.updateDetailChildren = function(parentName, parentValues) {
+    const detailChildren = this.el.querySelectorAll(`[data-parent-field="${parentName}"]`);
 
-    const isPartnerEvent = (eventTypeInput?.value === 'partner');
-    eventOrgFieldWrapper.classList.toggle('hidden', !isPartnerEvent);
-    eventOrgInput.required = isPartnerEvent;
+    for (const child of detailChildren) {
+      const childWrapper = this.el.querySelector(`.place-${child.name}-field`);
+      const targetParentValue = child.getAttribute('data-parent-value');
+      const isChildValueSelected = (parentValues.includes(targetParentValue));
+      childWrapper.classList.toggle('hidden', !isChildValueSelected);
+      child.required = isChildValueSelected;
+    }
   }
 
-  Shareabouts.PlaceFormView.prototype.initSubmissionEventType = function() {
-    this.updateSubmissionEventFields();
+  Shareabouts.PlaceFormView.prototype.initDetailChildren = function() {
+    const detailParents = this.el.querySelectorAll('[data-has-detail-children]');
+    const parentNames = [...new Set(Array.from(detailParents).map((p) => p.name))];
+    for (const parentName of parentNames) {
+      const parentValues = Array.from(detailParents).filter((p) => p.name === parentName && p.checked).map((p) => p.value);
+      this.updateDetailChildren(parentName, parentValues);
+    }
   }
 
-  Shareabouts.PlaceFormView.prototype.onSubmissionEventTypeChange = function() {
-    this.updateSubmissionEventFields();
-  };
+  Shareabouts.PlaceFormView.prototype.onDetailParentChange = function(event) {
+    const parent = event.target;
+    const parentName = parent.name;
+    const parentValues = Array.from(this.el.querySelectorAll(`[name="${parentName}"]:checked`)).map((p) => p.value);
+    this.updateDetailChildren(parentName, parentValues);
+  }
 
   const Shareabouts_PlaceFormView_render = Shareabouts.PlaceFormView.prototype.render;
   Shareabouts.PlaceFormView.prototype.render = function() {
     Shareabouts_PlaceFormView_render.apply(this, arguments);
 
-    this.initSubmissionEventType();
+    this.initDetailChildren();
 
     return this;
   }
