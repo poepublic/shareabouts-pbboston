5ac5d197070ed46517852d892e9f43b9ee523242
Anna Duan <annaduan@sas.upenn.edu>
2026-05-25 14:05:36 -0400

    Working legend
    Still needs:
    - better positioning
    - better colors (white background + colored icons?)
    - open from bottom of page, expand upward

---
+++ src/flavors/cycle3/static/js/idea-legend-ext.js
@@ -0,0 +1,105 @@
+// Provide a way to loop through location types in a template.
+Handlebars.registerHelper('each_place_type', function() {
+    const args = Array.from(arguments);
+    const options = args.slice(-1)[0];
+    const exclusions = args.slice(0, args.length-1);
+  
+    let result = '';
+    for (const [type, typeOptions] of Object.entries(Shareabouts.Config.placeTypes)) {
+      const data = {
+        type,
+        ...typeOptions
+      };
+  
+      // if not an exclusion
+      if (!exclusions.includes(type)) {
+        result += options.fn(data);
+      }
+    }
+  
+    return result;
+  });
+
+  // Provide a way to determine whether a given place type is visible (i.e. not
+// filtered out).
+Handlebars.registerHelper('is_place_type_shown', function(type, options) {
+    const path = window.location.pathname;
+    const isFiltered = path.includes('/filter/') && !path.includes('/filter/all');
+    const filterType = path.split('/').pop();
+  
+    const data = {isFiltered, ...this};
+    return (!isFiltered || filterType === type) ? options.fn(data) : options.inverse(data);
+  });
+  
+  // Provide a way to count the number of places for a given location type.
+  Handlebars.registerHelper('count_places', function(type) {
+    if (!window.app) {
+      return '…';
+    }
+  
+    const places = window.app.collection.models;
+    if (type) {
+      return places.filter(place => place.get('location_type') === type).length;
+    } else {
+      return places.length;
+    }
+  });
+
+
+
+// Update the current count of places in the legend.
+Shareabouts.AppView.prototype.renderLegend = function() {
+    const legendWrapper = this.el.querySelector('.legend-wrapper');
+    if (!legendWrapper) {
+      return;
+    }
+    const html = Handlebars.templates['legend']();
+    legendWrapper.innerHTML = html;
+  };
+  
+  Shareabouts.AppView.prototype.toggleLegend = function() {
+    const legendWrapper = this.el.querySelector('.legend-wrapper');
+    legendWrapper.classList.toggle('open');
+  }
+  
+  var original_AppView_initialize = Shareabouts.AppView.prototype.initialize;
+  Shareabouts.AppView.prototype.initialize = function() {
+    const result = original_AppView_initialize.call(this, ...arguments);
+
+    const legendWrapper = document.createElement('div');
+    legendWrapper.className = 'legend-wrapper';
+    document.getElementById('map-container').append(legendWrapper);
+
+    this.renderLegend();
+  
+    // Use app.collection events to trigger a legend update.
+    this.boundRenderLegend = _.debounce(this.renderLegend, 0, false).bind(this);
+    this.collection.on('add remove reset', this.boundRenderLegend);
+  
+    // Handle the legend toggle button.
+    this.handleLegendToggleClick = (evt) => {
+      if (evt.target.closest('.legend-toggle')) {
+        this.toggleLegend();
+      }
+    };
+    this.el.addEventListener('click', this.handleLegendToggleClick);
+    return result;
+  }
+  
+  // Update the current place type in the activity view.
+  Shareabouts.AppView.prototype.setSelectedPlaceType = function(type) {
+    this.renderLegend();
+  }
+  
+  var original_App_setLocationTypeFilter = Shareabouts.App.prototype.setLocationTypeFilter;
+  Shareabouts.App.prototype.setLocationTypeFilter = function(type) {
+    const result = original_App_setLocationTypeFilter.call(this, ...arguments);
+    this.appView.setSelectedPlaceType(type);
+    return result;
+  }
+
+  Shareabouts.AppView.prototype.onClickClosePanelBtn = function(evt) {
+    evt.preventDefault();
+    Shareabouts.Util.log('USER', 'panel', 'close-btn-click');
+    this.options.router.navigate('/', {trigger: true});
+  };
