e64cd352a725253a40f4df774cfb7d45782ff626
Anna Duan <annaduan@sas.upenn.edu>
2026-08-16 20:15:04 -0400

    Revert "3 cases for ballot banner text and ballot checkbox when user is verified"
    This reverts commit aa9ea65e1df4f17a21f18302cd78bf9168c2cd02.

--- src/sa_vote/static/sa_vote/js/views/ballot-view.js
+++ src/sa_vote/static/sa_vote/js/views/ballot-view.js
@@ -1,38 +1,6 @@
-const MAX_SELECTIONS = 5;
-
 export const BallotView = Backbone.View.extend({
-    events: {
-      'change .proposal-checkbox': 'updateSelectionCount',
-    },
-
     render: function() {
       this.$el.html(Handlebars.templates['sa_vote/pages/ballot'](this.options));
-      this.updateSelectionCount();
       return this;
-    },
-
-    updateSelectionCount: function() {
-      const count = this.$('.proposal-checkbox:checked').length;
-      const remaining = MAX_SELECTIONS - count;
-
-      if (this.options.verified) {
-        this.$('.proposal-checkbox:not(:checked)').prop('disabled', count >= MAX_SELECTIONS);
-      }
-
-      // Case 1: no proposals selected
-      if (count === 0) {
-        this.$('#ballot-banner-count').text(`Select up to ${MAX_SELECTIONS} proposals`);
-        this.$('#ballot-banner-subtext').text('');
-      // Case 2: 1-4 proposals selected
-      } else if (0 < count && count < MAX_SELECTIONS) {
-        this.$('#ballot-banner-count').text(`${count} proposals selected`);
-        this.$('#ballot-banner-subtext').text(`You may pick up to ${remaining} more`);
-      } else {
-      // Case 3: 5 (max) proposals selected
-        this.$('#ballot-banner-count').text(`${MAX_SELECTIONS} proposals selected`);
-        this.$('#ballot-banner-subtext').text('Review your vote');
-      }
-
-
-    },
+    }
   });
