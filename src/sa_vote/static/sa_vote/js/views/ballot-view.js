6413dc8c9aa71c1644240c317ba367225aa95e23
Anna Duan <annaduan@sas.upenn.edu>
2026-08-20 19:51:00 -0400

    Move ballot banner html to a separate file so the strings can be translated

--- src/sa_vote/static/sa_vote/js/views/ballot-view.js
+++ src/sa_vote/static/sa_vote/js/views/ballot-view.js
@@ -6,6 +6,16 @@ export const BallotView = Backbone.View.extend({
       'click .selected-proposal': 'removeSelection',
     },
 
+    getTemplateContext: function(count, remaining) {
+      return _.extend({
+        count: count,
+        remaining: remaining,
+        MAX_SELECTIONS: MAX_SELECTIONS,
+        ballotEmpty: count === 0,
+        ballotFull: count === MAX_SELECTIONS,
+      }, this.options);
+    },
+
     render: function() {
       this.$el.html(Handlebars.templates['sa_vote/pages/ballot'](this.options));
       this.updateSelectionCount();
@@ -20,25 +30,9 @@ export const BallotView = Backbone.View.extend({
         this.$('.proposal-checkbox:not(:checked)').prop('disabled', count >= MAX_SELECTIONS);
       } else { this.$('.selected-proposals').text(''); }
 
-
-      // Update the banner text based on number of selections
-      // Case 1: no proposals selected
-      if (count === 0) {
-        this.$('#ballot-banner-count').text(`Select up to ${MAX_SELECTIONS} proposals`);
-        this.$('#ballot-banner-subtext').text('');
-        this.$('.ballot-banner-verified-summary').addClass('no-proposal-selections');
-        this.$('.ballot-vote-button').empty();
-      // Case 2: 1-4 proposals selected
-      } else if (0 < count && count < MAX_SELECTIONS) {
-        this.$('#ballot-banner-count').text(`${count} proposal${count === 1 ? '' : 's'} selected`);
-        this.$('#ballot-banner-subtext').text(`You may pick up to ${remaining} more`);
-        this.$('.ballot-vote-button').html('<button class="btn btn-primary" id="submit-ballot">Vote</button>');
-      } else {
-      // Case 3: 5 (max) proposals selected
-        this.$('#ballot-banner-count').text(`${MAX_SELECTIONS} proposals selected`);
-        this.$('#ballot-banner-subtext').text('Ready to vote?');
-      }
-
+      const ballotBannerTemplate = Handlebars.templates['sa_vote/includes/ballot-banner'];
+      const context = this.getTemplateContext(count, remaining);
+      this.$('.ballot-banner-container').html(ballotBannerTemplate(context))
 
       // Insert / update selected proposals list
       const $list = this.$('.selected-proposals');
